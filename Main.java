7d32713dcfe1ef0b1ac1166ff83c3b458b52392b
Ionuț <ionut.cristea@outlook.com>
2020-09-28 17:30:05 +0300

    Extract parameters from program arguments

--- Main.java
+++ Main.java
@@ -1,41 +1,49 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
-
 @FunctionalInterface
 interface CypherFunc {
-    public char apply(char letter, int key);
+    char apply(char letter, int key);
 }
 
 public class Main {
     public static void main(String[] args) {
-        var input = new Scanner(System.in);
-
-        String mode = input.nextLine();
-        String message = input.nextLine();
-        int key = input.nextInt();
+        String mode = extract(args, "MODE", "enc");
+        int key = Integer.parseInt(extract(args, "KEY", "0"));
+        String message = extract(args, "DATA", "");
 
         String cyphertext = getCypheredMessage(message.toCharArray(), key, mode);
 
         System.out.println(cyphertext);
     }
 
+    private static String extract(String[] args, String arg, String defaultValue) {
+        for (int i = 0; i < args.length - 1; i += 2) {
+            if (args[i].substring(1).toLowerCase().equals(arg.toLowerCase())) {
+                return args[i + 1];
+            }
+        }
+
+        return defaultValue;
+    }
+
     private static String getCypheredMessage(char[] letters, int key, String mode) {
-        String cyphertext = null;
+        String cyphertext;
         switch (mode.toUpperCase()) {
-            case "ENC":
-                cyphertext = getCypheredMessage(letters, key, Main::encrypt);
-            break;
             case "DEC":
                 cyphertext = getCypheredMessage(letters, key, Main::decrypt);
-            break;
+                break;
+            case "ENC":
+                cyphertext = getCypheredMessage(letters, key, Main::encrypt);
+                break;
+            default:
+                cyphertext = null;
         }
+
         return cyphertext;
     }
 
     private static String getCypheredMessage(char[] letters, int key, CypherFunc func) {
         var encryptedChars = new char[letters.length];
-
         for (int i = 0; i < letters.length; i++) {
             encryptedChars[i] = func.apply(letters[i], key);
         }
@@ -44,18 +52,10 @@ public class Main {
     }
 
     private static char encrypt(char letter, int shiftedPos) {
-        char encryptedLetter = (char) (letter + shiftedPos);
-        /*if (encryptedLetter > 'z') {
-            int delta = encryptedLetter - 'z';
-            encryptedLetter = (char) ('a' - 1 + delta);
-        }*/
-
-        return encryptedLetter;
+        return (char) (letter + shiftedPos);
     }
 
     private static char decrypt(char letter, int shiftedPos) {
-        char encryptedLetter = (char) (letter - shiftedPos);
-
-        return encryptedLetter;
+        return (char) (letter - shiftedPos);
     }
 }

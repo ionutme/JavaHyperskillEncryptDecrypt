3afa2d44967f771373b9a2a4a0665ba7c88877ce
Ionuț <ionut.cristea@outlook.com>
2020-10-19 16:37:25 +0300

    Add draw.io diagram

--- CipherProcessor.java
+++ CipherProcessor.java
@@ -5,20 +5,20 @@ public class CipherProcessor {
     private final AbstractEncoderDecoder processor;
 
     CipherProcessor(String mode, String type) {
-        this.processor = getCipher(mode, type);
+        this.processor = getEncoderDecoder(mode, type);
     }
 
     public String process(CrypticMessage message) {
         return processor.cypher(message);
     }
 
-    private AbstractEncoderDecoder getCipher(String mode, String type) {
-        EncoderDecoderFactory factory = getCipherFactory(mode);
+    private AbstractEncoderDecoder getEncoderDecoder(String mode, String type) {
+        EncoderDecoderFactory factory = getEncoderDecoderFactory(mode);
 
         return factory.create(type);
     }
 
-    private EncoderDecoderFactory getCipherFactory(String type) {
+    private EncoderDecoderFactory getEncoderDecoderFactory(String type) {
 
         EncoderDecoderFactory factory;
         switch (type.toUpperCase()) {
